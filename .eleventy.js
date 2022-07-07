bddf3031c949bcd1876f1e7a0a24238b38152366
santiago <santiagotaschetta@gmail.com>
2022-07-06 23:35:07 -0300

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const sass = require('sass')
+const path = require('node:path')
+
+module.exports = (eleventyConfig) => {
+
+  eleventyConfig.addTemplateFormats('scss')
+
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css',
+    compile: (inputContent, inputPath) => {
+      const parsed = path.parse(inputPath)
+
+      if(parsed.name.startsWith('_')) {
+        return;
+      }
+      
+      const result = sass.compileString(inputContent, {
+        loadPaths: [
+          parsed.dir || '.'
+        ]
+      })
+
+      return async () => {
+        return result.css
+      }
+    }
+  })
+
+  return {
+    dir: {
+      input: 'source',
+      output: 'public',
+      layouts: '_layouts',
+    }
+  }  
+}
